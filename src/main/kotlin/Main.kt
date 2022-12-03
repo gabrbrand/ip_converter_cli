02ac3d1a0ee25cba53e8d51477d3cf455941ae3a
Gabriel Brand <gabr.brand@gmail.com>
2022-12-03 14:11:17 +0100

    Check answers and IP addresses before using them

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,25 +1,86 @@
 import java.lang.Integer.toBinaryString
 
+val validDecimalCharacters = listOf('0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.')
+val validBinaryCharacters = listOf('0', '1', '.')
+
 fun main() {
     do {
         convertIpAddress()
 
         println("\u001B[34mConvert another IP address? Yes (1) No (Any other number)\u001B[0m")
-        val answer = readln().toInt()
+        val answer = getAnswer()
 
         if (answer != 1) println("\u001B[33mThank you for using IP Converter!\u001B[0m")
     } while (answer == 1)
+}
+
+fun getAnswer(): Int {
+    var answer: String
+    do {
+        answer = readln()
+        checkAnswer(answer)
+    } while (!isValidAnswer(answer))
+    return answer.toInt()
+}
+
+fun checkAnswer(answer: String) {
+    if (!answer.all { it.isDigit() }) {
+        println("\u001B[31mThe entered answer isn't a number!\u001B[0m")
+    }
+}
+
+fun isValidAnswer(answer: String): Boolean = answer.all { ch -> ch.isDigit() }
+
+fun getIpAddress(type: String): String {
+    var ipAddress: String
+    do {
+        ipAddress = readln()
+        checkIpAddress(ipAddress, type)
+    } while (!isValidIpAddress(ipAddress, type))
+    return ipAddress
+}
+
+fun checkIpAddress(ipAddress: String, type: String) {
+    when (type) {
+        "decimal" -> {
+            if (!ipAddress.all { it in validDecimalCharacters }) {
+                val invalidDecimalCharacters = ipAddress.filter { ch -> ch !in validDecimalCharacters }
+                println(
+                    "\u001B[31mThe entered IP address contains invalid characters (${
+                        invalidDecimalCharacters.toList().joinToString()
+                    })!\u001B[0m"
+                )
+            }
+        }
+
+        "binary" -> {
+            if (!ipAddress.all { it in validBinaryCharacters }) {
+                val invalidBinaryCharacters = ipAddress.filter { ch -> ch !in validBinaryCharacters }
+                println(
+                    "\u001B[31mThe entered IP address contains invalid characters (${
+                        invalidBinaryCharacters.toList().joinToString()
+                    })!\u001B[0m"
+                )
+            }
+        }
+    }
+}
+
+fun isValidIpAddress(ipAddress: String, type: String): Boolean = when (type) {
+    "decimal" -> ipAddress.all { it in validDecimalCharacters }
+    "binary" -> ipAddress.all { it in validBinaryCharacters }
+    else -> false
 }
 
 fun convertIpAddress() {
     println("\u001B[34mConvert IP address to binary (1) or decimal (2)?\u001B[0m")
 
     do {
-        val answer = readln().toInt()
+        val answer = getAnswer()
         when (answer) {
             1 -> {
                 println("\u001B[33mEnter a decimal IP address:\u001B[0m")
-                val decimalIpAddress = readln()
+                val decimalIpAddress = getIpAddress("decimal")
 
                 val binaryIpAddress = convertToBinaryIpAddress(decimalIpAddress)
 
@@ -28,14 +89,14 @@ fun convertIpAddress() {
 
             2 -> {
                 println("\u001B[33mEnter a binary IP address:\u001B[0m")
-                val binaryIpAddress = readln()
+                val binaryIpAddress = getIpAddress("binary")
 
                 val decimalIpAddress = convertToDecimalIpAddress(binaryIpAddress)
 
                 println("\u001B[32mDecimal IP address:\n\u001B[0m\u001B[92m$decimalIpAddress\u001B[0m")
             }
 
-            else -> println("\u001B[31mEnter a valid number!\u001B[0m")
+            else -> println("\u001B[31mEnter a valid number (1 or 2)!\u001B[0m")
         }
     } while (answer !in listOf(1, 2))
 }
--- src/test/kotlin/MainKtTest.kt
+++ src/test/kotlin/MainKtTest.kt
@@ -6,16 +6,16 @@ internal class MainKtTest {
     @Test
     @DisplayName("Convert a decimal to a binary IP address")
     fun testConvertToBinaryIpAddress() {
-        assertEquals("11000000.10100010.01010101.10011000", convertToBinaryIPAddress("192.162.85.152"))
-        assertEquals("00001010.00001111.00001000.00000011", convertToBinaryIPAddress("10.15.8.3"))
-        assertEquals("11111111.11111111.11111111.00000000", convertToBinaryIPAddress("255.255.255.0"))
+        assertEquals("11000000.10100010.01010101.10011000", convertToBinaryIpAddress("192.162.85.152"))
+        assertEquals("00001010.00001111.00001000.00000011", convertToBinaryIpAddress("10.15.8.3"))
+        assertEquals("11111111.11111111.11111111.00000000", convertToBinaryIpAddress("255.255.255.0"))
     }
 
     @Test
     @DisplayName("Convert a binary to a decimal IP address")
     fun testConvertToDecimalIPAddress() {
-        assertEquals("192.162.85.152", convertToDecimalIPAddress("11000000.10100010.01010101.10011000"))
-        assertEquals("10.15.8.3", convertToDecimalIPAddress("00001010.00001111.00001000.00000011"))
-        assertEquals("255.255.255.0", convertToDecimalIPAddress("11111111.11111111.11111111.00000000"))
+        assertEquals("192.162.85.152", convertToDecimalIpAddress("11000000.10100010.01010101.10011000"))
+        assertEquals("10.15.8.3", convertToDecimalIpAddress("00001010.00001111.00001000.00000011"))
+        assertEquals("255.255.255.0", convertToDecimalIpAddress("11111111.11111111.11111111.00000000"))
     }
 }
